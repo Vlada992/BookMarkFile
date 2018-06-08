f9176af229fda566ef80e1109367f44447d9ee23
Vladan92 <vladanng@gmail.com>
2018-06-08 02:18:54 +0200

    'bookmarke'

--- js/main.js
+++ js/main.js
@@ -1,31 +1,51 @@
-
-function showCoords(event){
-  console.log('y:', event.pageY,  "x:",event.pageX)
-}
-
-
-$("#stajlBukIc").mouseover(() => {
-   document.getElementById('stajlBukIc').src = `images/filledBuk1.png`;
-   $("#bokmarkDef").fadeIn(820);
-    document.getElementById('bokmarkDef').style.display ='block'
+$("#stajlBukIc").mouseenter(() => {
+    $("#bokmarkDef").fadeIn(820);
+    document.getElementById('stajlBukIc').src = `images/filledBuk1.png`;
+    document.getElementById('bokmarkDef').style.display = 'block'
 });
+
 $("#stajlBukIc").mouseleave(() => {
-  document.getElementById('stajlBukIc').src = `images/bukImg1.png`;
-   $("#bokmarkDef").fadeOut(1000);
+    document.getElementById('stajlBukIc').src = `images/bukImg1.png`;
+    $("#bokmarkDef").fadeOut(1000);
 });
+
+
+$("#allDel").mouseenter(() => {
+    document.getElementById('allDelDef').style.opacity = '1'
+    //$("#allDelDef").fadeIn(920);
+});
+
+$("#allDel").mouseleave(() => {
+    document.getElementById('allDelDef').style.opacity = '0'
+    //$("#allDelDef").fadeOut(300);
+});
+
+
+$("#allLine").mouseenter(() => {
+    document.getElementById('allDelDef3').style.opacity = '1'
+    //$("#allDelDef").fadeIn(920);
+});
+
+$("#allLine").mouseleave(() => {
+    document.getElementById('allDelDef3').style.opacity = '0'
+    //$("#allDelDef").fadeOut(300);
+});
+
 
 
 document.getElementById("myForm").addEventListener("submit", saveBookmark);
 
-function saveBookmark(e){
+function saveBookmark(e) {
     document.getElementById('myGifBuk').style.display = 'block'
-    setTimeout(()=> {
-     document.getElementById('myGifBuk').style.display = 'none'
+    setTimeout(() => {
+        document.getElementById('myGifBuk').style.display = 'none'
     }, 2000)
 
     var siteNameV = document.getElementById("siteName").value;
-    var siteUrlV = document.getElementById("siteUrl").value; 
-    if (!validateForm(siteNameV, siteUrlV)){
+    let siteJustUrl = document.getElementById('siteUrl');
+    var siteUrlV = document.getElementById("siteUrl").value;
+    console.log('a ovde, vrednost:', siteUrlV);
+    if (!validateForm(siteNameV, siteUrlV)) {
         return false;
     }
     var siteNameDOM = document.getElementById("siteName");
@@ -33,12 +53,16 @@ function saveBookmark(e){
     siteUrlDOM.style.font = 'normal  bold 20px';
     siteNameDOM.style.font = 'normal  bold 20px';
 
+
     if (siteUrlV.charAt(0) == 'w') {
         siteUrlV = 'http://' + siteUrlV;
     }
+    var tajmIt = String(new Date());
+    var tajm1 = tajmIt.slice(0, 24);
     var bookmark = {
         name: siteNameV.toUpperCase(),
-        url: siteUrlV
+        url: siteUrlV,
+        time: tajm1
     };
 
     if (localStorage.getItem("bookmarks") === null) {
@@ -50,12 +74,15 @@ function saveBookmark(e){
         bookmarks.push(bookmark);
         localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
     }
+
     document.getElementById("myForm").reset();
+    //fetchBookmarks()
     fetchBookmarks();
+    fetchBookInline()
     e.preventDefault();
 }
 
-function deleteBookmark(url, name){
+function deleteBookmark(url, name) {
     var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
     for (var i = 0; i < bookmarks.length; i++) {
         if (bookmarks[i].url == url && bookmarks[i].name == name) {
@@ -64,44 +91,93 @@ function deleteBookmark(url, name){
     }
     localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
     fetchBookmarks();
+    fetchBookInline()
+
 };
 
-function deleteAllBook(){
-   var bookmarks2 = JSON.parse(localStorage.getItem("bookmarks"));
-       for (var i = 0; i < bookmarks2.length; i++) {
+function deleteAllBook() {
+    var bookmarks2 = JSON.parse(localStorage.getItem("bookmarks"));
+    for (var i = 0; i < bookmarks2.length; i++) {
         bookmarks2.splice(i);
-       }
-       localStorage.setItem("bookmarks", JSON.stringify(bookmarks2));
+    }
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks2));
     fetchBookmarks()
+    fetchBookInline()
 };
 
-////////////////////////////////////////////////////////////////////////
-function fetchBookmarks(){
+function fetchBookmarks() {
     var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
     var bookmarksResults;
-    for (var x = 0; x <= 2; x++) {
-        bookmarksResults = document.getElementById("bookmarksResults");
-    }
+    bookmarksResults = document.getElementById("bookmarksResults");
+
+
     bookmarksResults.innerHTML = "";
-    for (var i = 0; i < bookmarks.length; i++){
+    for (var i = 0; i < bookmarks.length; i++) {
         var name = bookmarks[i].name;
         var url = bookmarks[i].url;
-  bookmarksResults.innerHTML +=`
+        var tajm2 = bookmarks[i].time;
+
+        bookmarksResults.innerHTML += `
     <div class="thumbnail">
-      <img id ='imgIdBuk' src= 'http://api.screenshotlayer.com/api/capture?access_key=99359d2c1a52b834f4115b4e13a71262&url=${url}&width=400' />
+      <img id ='imgIdBuk' src= 'http://api.screenshotlayer.com/api/capture?access_key=99359d2c1a52b834f4115b4e13a71262&url=${url}&width=370' />
       <div class="caption">
-      <h3 title='Name of marked link' id =btnH3Id> <img id='imgIdFav' src='https://plus.google.com/_/favicon?domain_url=${url}'/>&nbsp;${name}&nbsp;&nbsp;&nbsp;&nbsp;<a><i title="See latest full capture of page" onclick= callIt("${url}") class="fa fa-camera-retro"></i></a></h3>
+      <h3 title='Wbesite name is: ${name}' id =btnH3Id> <img id='imgIdFav' src='https://plus.google.com/_/favicon?domain_url=${url}'/>&nbsp;${name}&nbsp;&nbsp;&nbsp;&nbsp;<a><i title="See latest full capture of page" onclick= callIt("${url}") class="fa fa-camera-retro"></i></a></h3>
         <p></p>` +
             '<a href="' + url + '" target=_"blank" class="btn btn-info" role="button">Visit Us &nbsp; <i class="fas fa-external-link-alt"></i> </a></a> <a href="#" onclick="deleteBookmark(\'' + url + '\', \'' + name + '\')" class="btn btn-danger" role="button">Delete &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fas fa-trash-alt"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +
-           `</div>
-</div>
-`
+            `</div>
+<p id='stajlTajmer'>${tajm2}</p>
+</div>`
     }
 };
-function validateForm(siteName, siteUrl){
+
+function fetchBookInline() {
+    var bookmarksGet = JSON.parse(localStorage.getItem("bookmarks"));
+    var tempSwap = false;
+    var bukRes = document.getElementById("bookmarksResults");
+    var lineTxt = document.getElementById('allLine');
+
+    lineTxt.addEventListener("click", () => {
+        if (tempSwap === false) {
+            bukRes.innerHTML = '';
+            for (var i = 0; i < bookmarksGet.length; i++) {
+                var nameG = bookmarksGet[i].name;
+                var urlG = bookmarksGet[i].url;
+                var tajm2G = bookmarksGet[i].time;
+                bukRes.innerHTML += `<div class="caption1">
+      <h3 title='Website name is: ${nameG}' id =btnH3Id1> <img id='imgIdFav1' src='https://plus.google.com/_/favicon?domain_url=${urlG}'/>&nbsp;${nameG.toLowerCase()}&nbsp;&nbsp;&nbsp;</h3> <a id='retroI'><i title="See latest full capture of page" onclick= callIt("${urlG}") class="fa fa-camera"></i>  </a>
+        <p></p>` +
+                    '<a id="visitBtn1" href="' + urlG + '" target=_"blank" class="btn btn-primary" role="button">Visit Us &nbsp; <i class="fas fa-external-link-alt"></i> </a> <a id="deleteBtn1" href="#" onclick="deleteBookmark(\'' + urlG + '\', \'' + nameG + '\')" class="btn btn-warning" role="button">Delete &nbsp;&nbsp;&nbsp;&nbsp;<i class="fas fa-trash"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +
+                    `<p id='stajlTajmer1'>${tajm2G}</p>
+
+</div> `
+            }
+            tempSwap = true;
+        } else {
+            bukRes.innerHTML = '';
+            for (var i = 0; i < bookmarksGet.length; i++) {
+                var nameG = bookmarksGet[i].name;
+                var urlG = bookmarksGet[i].url;
+                var urlG1 = /\.([^\.]+)\./g.exec(String(bookmarksGet[i].url))
+                console.log(urlG, urlG1);
+                var tajm2G = bookmarksGet[i].time;
+                bukRes.innerHTML += `    <div class="thumbnail">
+<img id ='imgIdBuk' src= 'http://api.screenshotlayer.com/api/capture?access_key=99359d2c1a52b834f4115b4e13a71262&url=${urlG}&width=370'/>
+<div class="caption">
+      <h3 title='Websate name is: ${nameG}' id =btnH3Id> <img id='imgIdFav' src='https://plus.google.com/_/favicon?domain_url=${urlG}'/>&nbsp;${nameG}&nbsp;&nbsp;&nbsp;&nbsp;<a><i title="See latest full capture of page" onclick= callIt("${urlG}") class="fa fa-camera-retro"></i></a></h3>
+     <p></p>` + '<a href="' + urlG + '" target=_"blank" class="btn btn-info" role="button">Visit Us &nbsp; <i class="fas fa-external-link-alt"></i> </a> <a href="#" onclick="deleteBookmark(\'' + urlG + '\', \'' + nameG + '\')" class="btn btn-danger" role="button">Delete &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fas fa-trash-alt"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +
+                    `</div> 
+<p id='stajlTajmer'>${tajm2G}</p>`
+            }
+            tempSwap = false;
+        };
+    });
+};
+
+
+function validateForm(siteName, siteUrl) {
     var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
     var regex = new RegExp(expression);
-    if (!siteName && !siteUrl){
+    if (!siteName && !siteUrl) {
         alert("Please fill Website Name and Website Url!");
         return false;
     } else if (!siteUrl) {
@@ -117,16 +193,20 @@ function validateForm(siteName, siteUrl){
         alert('Please use valid Name, not URL')
         return false;
     }
-    if (!siteUrl.match(regex)){
+    if (!siteUrl.match(regex)) {
         alert("Please use valid URL!!!");
         return false;
     }
     return true;
-} 
-function callIt(takeUrl) {
+}
+function callIt(takeUrl){
     var image = new Image();
-    image.src = window.open(`http://api.screenshotlayer.com/api/capture?access_key=99359d2c1a52b834f4115b4e13a71262&url=${takeUrl}&viewport=1440x900&fullpage=1`);
+    var imgUrl = `http://api.screenshotlayer.com/api/capture?access_key=99359d2c1a52b834f4115b4e13a71262&url=${takeUrl}&viewport=1440x900&fullpage=1`
+    /*document.getElementById('imgTester').src = imgUrl;
     image.onload = function(e) {
-    }
+        console.log('a', e)
+    }*/
+    var firstTake = window.open(`http://api.screenshotlayer.com/api/capture?access_key=99359d2c1a52b834f4115b4e13a71262&url=${takeUrl}&viewport=1440x900&fullpage=1`)
+    var fixUrl = 'https' + takeUrl.slice(4)
+    image.src = firstTake
 };
-
